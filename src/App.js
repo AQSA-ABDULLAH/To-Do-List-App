c228f5bd78852fba901516c715353b5ab335d0d2
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-13 15:35:07 +0500

    add task to inprogress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [Pending, setPending] = useState([]);
   const [Inprogress, setInprogress] = useState([]);
   const [Completed, setCompleted] = useState([]);
   const [input, setInput] = useState('');
@@ -16,9 +15,28 @@ function App() {
     setTodos([todo, ...todos]);
   }
 
+  const addToProgress = (id) => {
+    const item = todos.find(x => x.id === id);
+    setInprogress([item, ...Inprogress]);
+    const filterarray = todos.filter(x => x.id !== id);
+    setTodos([filterarray]);
+  }
+
+  const deleteTodo = (id) => {
+    const filterarray = todos.filter(x => x.id !== id);
+    setTodos([filterarray]);
+  }
+
+  const addToCompleted = (id) => {
+    const item = Inprogress.find(x => x.id === id);
+    setCompleted([item, ...Completed]);
+    const filterarray = Inprogress.filter(x => x.id !== id);
+    setTodos([filterarray]);
+  }
+
   useEffect(() => {
 
-  }, [todos])
+  }, [todos, Inprogress, Completed])
 
   return (
     <div className="App">
@@ -31,39 +49,34 @@ function App() {
 
         <div className='todos_wrapper'>
           <div className='todos_list'>
-            <h3 className='todo_title'>All Todos List</h3>
+            <h3 className='todo_title'>Pending List</h3>
             {todos.map((item, index) =>
               <div className='todo_card' key={item.id}>
                 <p className="card_text">{item.text}</p>
-                <FaCheck className="icon-check-todo" />
+                <FaCheck className="icon-check-todo" onClick={() => addToProgress(item.id)} />
                 <FaTrash className="icon-trash-todo" />
               </div>
             )}
 
           </div>
           <div className='todos_list'>
-            <h3 className='todo_title'>Pending</h3>
-            <div className='Pending_card'>
-              <p className="card_text">donskjfjsfk</p>
-              <FaCheck className="icon-check-todo" />
-              <FaTrash className="icon-trash-todo" />
-            </div>
-          </div>
-          <div className='todos_list'>
             <h3 className='todo_title'>Inprogress</h3>
-            <div className='Inprogress_card'>
-              <p className="card_text">donskjfjsfk</p>
-              <FaCheck className="icon-check-todo" />
-              <FaTrash className="icon-trash-todo" />
-            </div>
+            {Inprogress.map((item, index) =>
+              <div className='Inprogress_card' key={item.key}>
+                <p className="card_text">{item.text}</p>
+                <FaCheck className="icon-check-todo" onClick={() => addToCompleted(item.id)} />
+              </div>
+            )}
+
           </div>
           <div className='todos_list'>
             <h3 className='todo_title'>Completed</h3>
-            <div className='Completed_card'>
-              <p className="card_text">donskjfjsfk</p>
-              <FaCheck className="icon-check-todo" />
-              <FaTrash className="icon-trash-todo" />
-            </div>
+            {Completed.map((item, index) =>
+              <div className='Completed_card' key={item.key}>
+                <p className="card_text">{item.text}</p>
+                <FaCheck className="icon-check-todo" />
+              </div>
+            )}
           </div>
 
         </div>
