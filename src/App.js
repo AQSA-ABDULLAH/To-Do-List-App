e366f191c7e7ca9d1bb149d885a5451dfaea195f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-13 18:11:11 +0500

    Add Styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { FaCheck, FaTrash, FaPlusCircle } from "react-icons/fa"
 import React, { useState, useEffect } from 'react';
 
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [inProgress, setInProgress] = useState([]);
@@ -43,7 +44,13 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <h3>TO-DO List</h3>
+        <h3 className='title'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48" fill="none">
+            <path d="M17.8 18.1L10.4 25.4L6.2 21.3L4 23.5L10.4 29.9L20 20.3L17.8 18.1ZM17.8 5.10001L10.4 12.4L6.2 8.30001L4 10.5L10.4 16.9L20 7.30001L17.8 5.10001ZM17.8 31.1L10.4 38.4L6.2 34.3L4 36.5L10.4 42.9L20 33.3L17.8 31.1Z" fill="#3F51B5" />
+            <path d="M24 22H44V26H24V22ZM24 9H44V13H24V9ZM24 35H44V39H24V35Z" fill="#90CAF9" />
+          </svg>
+          To Do App
+        </h3>
         <form className='form_todo'>
           <input type="text" className='form-control' onChange={(e) => setInput(e.target.value)} value={input} name='text' />
           <button type="button" onClick={addTodo} className='btn'><FaPlusCircle className='icon' /></button>
