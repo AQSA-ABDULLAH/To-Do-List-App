f7ce5a4f3df858c267358b4ac289b185a0eb73bd
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-13 15:46:17 +0500

    Add task to completed

--- src/App.js
+++ src/App.js
@@ -4,47 +4,49 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [Inprogress, setInprogress] = useState([]);
-  const [Completed, setCompleted] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [completed, setCompleted] = useState([]);
   const [input, setInput] = useState('');
+
   const addTodo = () => {
     const todo = {
       id: Math.floor(Math.random() * 1000),
       text: input
     }
     setTodos([todo, ...todos]);
+    setInput('');
   }
 
   const addToProgress = (id) => {
     const item = todos.find(x => x.id === id);
-    setInprogress([item, ...Inprogress]);
-    const filterarray = todos.filter(x => x.id !== id);
-    setTodos([filterarray]);
+    setInProgress([item, ...inProgress]);
+    const filteredTodos = todos.filter(x => x.id !== id);
+    setTodos(filteredTodos);
   }
 
   const deleteTodo = (id) => {
-    const filterarray = todos.filter(x => x.id !== id);
-    setTodos([filterarray]);
+    const filteredTodos = todos.filter(x => x.id !== id);
+    setTodos(filteredTodos);
   }
 
   const addToCompleted = (id) => {
-    const item = Inprogress.find(x => x.id === id);
-    setCompleted([item, ...Completed]);
-    const filterarray = Inprogress.filter(x => x.id !== id);
-    setTodos([filterarray]);
+    const item = inProgress.find(x => x.id === id);
+    setCompleted([item, ...completed]);
+    const filteredInProgress = inProgress.filter(x => x.id !== id);
+    setInProgress(filteredInProgress);
   }
 
   useEffect(() => {
 
-  }, [todos, Inprogress, Completed])
+  }, [todos, inProgress, completed])
 
   return (
     <div className="App">
       <div className='container'>
-        <h3>TO-D0 List</h3>
+        <h3>TO-DO List</h3>
         <form className='form_todo'>
-          <input type="text" className='form-control' onChange={(e) => setInput(e.target.value)} name='text' />
-          <button type="button" onClick={() => addTodo()} className='btn'><FaPlusCircle className='icon' /></button>
+          <input type="text" className='form-control' onChange={(e) => setInput(e.target.value)} value={input} name='text' />
+          <button type="button" onClick={addTodo} className='btn'><FaPlusCircle className='icon' /></button>
         </form>
 
         <div className='todos_wrapper'>
@@ -54,34 +56,30 @@ function App() {
               <div className='todo_card' key={item.id}>
                 <p className="card_text">{item.text}</p>
                 <FaCheck className="icon-check-todo" onClick={() => addToProgress(item.id)} />
-                <FaTrash className="icon-trash-todo" />
+                <FaTrash className="icon-trash-todo" onClick={() => deleteTodo(item.id)} />
               </div>
             )}
-
           </div>
           <div className='todos_list'>
-            <h3 className='todo_title'>Inprogress</h3>
-            {Inprogress.map((item, index) =>
-              <div className='Inprogress_card' key={item.key}>
+            <h3 className='todo_title'>In Progress</h3>
+            {inProgress.map((item, index) =>
+              <div className='inProgress_card' key={item.id}>
                 <p className="card_text">{item.text}</p>
                 <FaCheck className="icon-check-todo" onClick={() => addToCompleted(item.id)} />
               </div>
             )}
-
           </div>
           <div className='todos_list'>
             <h3 className='todo_title'>Completed</h3>
-            {Completed.map((item, index) =>
-              <div className='Completed_card' key={item.key}>
+            {completed.map((item, index) =>
+              <div className='completed_card' key={item.id}>
                 <p className="card_text">{item.text}</p>
                 <FaCheck className="icon-check-todo" />
               </div>
             )}
           </div>
-
         </div>
       </div>
-
     </div>
   );
 }
